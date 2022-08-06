1c222168e82b772f95f0ad25f4fc88cf32fd5d22
PatatasDelPapa <43626645+PatatasDelPapa@users.noreply.github.com>
2022-08-06 16:19:33 -0400

    New Simulator Engine Benchmark and new docs

--- benchmarks/benches/main.rs
+++ benchmarks/benches/main.rs
@@ -5,6 +5,7 @@ use std::time::Duration;
 use desim_benchmark::simulation as desim_simulation;
 use simrs_benchmark::simulation as simrs_simulation;
 use simulator_benchmark::simulation as simulator_simulation;
+use sim_v2::simulation as sim_v2_simulation;
 
 fn bench(c: &mut Criterion) {
     let mut group = c.benchmark_group("simulation");
@@ -19,6 +20,10 @@ fn bench(c: &mut Criterion) {
 
         group.bench_with_input(BenchmarkId::new("simulator", limit), &limit, |b, &limit| {
             b.iter(|| simulator_simulation(black_box(limit)));
+        });
+        
+        group.bench_with_input(BenchmarkId::new("simulator_v2", limit), &limit, |b, &limit| {
+            b.iter(|| sim_v2_simulation(black_box(limit)));
         });
     }
     group.finish();
--- benchmarks/src/main.rs
+++ benchmarks/src/main.rs
@@ -1,10 +1,12 @@
 use desim_benchmark::simulation as desim_simulation;
 use simrs_benchmark::simulation as simrs_simulation;
 use simulator_benchmark::simulation as simulator_simulation;
+use sim_v2::simulation as sim_v2_simulation;
 
 fn main() {
     desim_simulation(50000.0);
     simrs_simulation(50000);
     simulator_simulation(50000);
+    sim_v2_simulation(50000);
 }
 
